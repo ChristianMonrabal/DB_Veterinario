2da8f0134d772466764c502335d6c502338e3236
Christian Monrabal <90915794+ChristianMonrabal@users.noreply.github.com>
2023-05-03 19:57:53 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -43,7 +43,12 @@ $result = $mysqli->query ($query);
 <!--Tabla Mascota-->
 <h2 onclick="toggleTable2()">Tabla Mascotas</h2>
 <?php
-$query = "SELECT * from Mascota";
+$query = "SELECT m.*, r.nom as raza, p.nom as propietario, v.nom as veterinario, h.* 
+FROM mascota m 
+INNER JOIN raça r ON m.Raça = r.id 
+INNER JOIN propietari p ON m.Propietari = p.DNI 
+INNER JOIN veterinari v ON m.Vet = v.id 
+INNER JOIN historial h ON m.Historial = h.Id_Historial";
 $result = $mysqli->query ($query);
 ?>
 <table id="miTabla2" style="display:none" class="table table-dark table-striped table-hover">
@@ -53,27 +58,35 @@ $result = $mysqli->query ($query);
     <th>Nombre</th>
     <th>Genero</th>
     <th>Especie</th>
+    <th>Raza</th>
+    <th>Fecha de nacimiento</th>
     <th>Propietario</th>
+    <th>Veterinario</th>
+    <th>Historial</th>
     <th>Editar</th>
     <th>Eliminar</th>
 </tr>
 </thead>
 <tbody>
-    <?php
-    if ($result->num_rows > 0) {
-    while($row = $result->fetch_array()) {
-        $fila="<tr>";
-        $fila=$fila."<td>" . $row["Xip"] . "</td>";
-        $fila=$fila."<td>" . $row["nom"] . "</td>";
-        $fila=$fila."<td>" . $row["genere"] . "</td>";
-        $fila=$fila."<td>" . $row["Especie"] . "</td>";
-        $fila=$fila."<td>" . $row["Propietari"] . "</td>";
-        $fila=$fila."<td><a href='./EditarDB/editaMasc.php".$row["Mascota"]."' class='btn btn-primary btn-sm my-2'>Editar</a></td>";
-        $fila=$fila."<td><a href='#' onclick='alertaBorrar(".$row["Mascota"].")' class='btn btn-danger btn-sm my-2'>Eliminar</a></td></tr>";
-        echo $fila;
-        }
-    }
-?>
+<?php
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_array()) {
+                        $fila="<tr>";
+                        $fila=$fila."<td>" . $row["Xip"] . "</td>";
+                        $fila=$fila."<td>" . $row["nom"] . "</td>";
+                        $fila=$fila."<td>" . $row["genere"] . "</td>";
+                        $fila=$fila."<td>" . $row["Especie"] . "</td>";
+                        $fila=$fila."<td>" . $row["raza"] . "</td>";
+                        $fila=$fila."<td>" . $row["Data_Naixement"] . "</td>";
+                        $fila=$fila."<td>" . $row["propietario"] . "</td>";
+                        $fila=$fila."<td>" . $row["veterinario"] . "</td>";
+                        $fila=$fila."<td>" . $row["Historial"] . "</td>";
+                        $fila=$fila."<td><a href='./EditarDB/editaMasc.php".$row["xip"]."' class='btn btn-primary btn-sm my-2'>Editar</a></td>";
+                        $fila=$fila."<td><a href='#' onclick='alertaBorrar(".$row["Xip"].")' class='btn btn-danger btn-sm my-2'>Eliminar</a></td></tr>";
+                        echo $fila;
+                    }
+                }
+                ?>
 <a href= "./Formularios/formAltaMasc.php" class="btn btn-primary my-2">Alta Mascota</a>
 
 <!--Tabla Veterinario-->
